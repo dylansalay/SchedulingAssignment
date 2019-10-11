890f4af8f1c2271000c21ca415c27f3c50e2c16c
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2019-10-11 15:04:33 -0700

    added onclick alert to cells

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -3,15 +3,24 @@ class ScheduleController < ApplicationController
     @locations = Location.all
     @technicians = Technician.all
     @work_orders  = WorkOrder.all
-    @time_of_day = ["06:00 AM", "07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "13:00 PM", "14:00 PM", "15:00 PM", "16:00 PM", "17:00 PM", "18:00 PM"]
+    @time_of_day = ["06:00 AM", "07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "13:00 PM", "14:00 PM", "15:00 PM", "16:00 PM"]
     
-    @work_order = WorkOrder.find_by_sql("
-    SELECT * from work_orders 
-    INNER JOIN technicians ON technicians.id = work_orders.technician_id 
-    INNER JOIN locations on locations.id = work_orders.location_id
-    ORDER BY work_orders.time")
-
-    # @schedule = WorkOrder.joins(:location, :technician).group(technician.id, :time).select("technician.id as technician_id, time, sum(duration) as sum_duration, sum(price) as sum_price, string_agg(concat(location.name, ', ', location.city, ', '), ', ') ") 
-
+    # The three queries below all produce the same result. 
+    
+    @work_order = WorkOrder.joins(:location, :technician).unscoped.group(:time, :id).select("*").order(:time)
+    
+    # @work_order = WorkOrder.find_by_sql("
+    # SELECT * from work_orders 
+    # INNER JOIN technicians ON technicians.id = work_orders.technician_id 
+    # INNER JOIN locations on locations.id = work_orders.location_id
+    # ORDER BY work_orders.time")
+    
+    # @work_order = WorkOrder.unscoped.find_by_sql("
+    # SELECT *
+    # FROM work_orders 
+    # INNER JOIN technicians ON technicians.id = work_orders.technician_id 
+    # INNER JOIN locations ON locations.id = work_orders.location_id
+    # GROUP BY work_orders.time, work_orders.id, technicians.id, locations.id
+    # ORDER BY work_orders.time")
   end
 end
